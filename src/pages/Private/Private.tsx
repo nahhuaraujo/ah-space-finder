8dd32a9464758a78b865c0dd68c3855a73534af0
Nahuel Araujo <nahuel.araujo@globant.com>
2022-10-04 16:17:32 -0300

    Add NotFound page to private pages

--- src/pages/Private/Private.tsx
+++ src/pages/Private/Private.tsx
@@ -1,6 +1,6 @@
 import { Route, Routes } from 'react-router-dom';
 import { Navbar } from '../../components';
-import { Home, Profile } from '../';
+import { Home, Profile, NotFound } from '../';
 
 const Private = () => {
   return (
@@ -10,6 +10,7 @@ const Private = () => {
         <Route path='/' element={<Home />} />
         <Route path='/dashboard' element={<h1>Soy el dashboard</h1>} />
         <Route path='/profile' element={<Profile />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </div>
   );
